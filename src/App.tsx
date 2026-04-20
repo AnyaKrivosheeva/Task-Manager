ca63d16cdae9db8dd999b981c01408f068558df3
Anya Krivosheeva <anyakrivosheeva98@gmail.com>
2026-04-20 21:18:19 +0300

    добавлен подсчет и визуализация статистики выполнения задач, подключен реакт роутер

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
 import AddTaskForm from "./components/AddTaskForm/AddTaskForm"
 import TaskList from "./components/TaskList/TaskList"
+import TaskStats from "./components/TaskStats/TaskStats"
+import { Routes, Route, Navigate, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Список твоих делишек</h1>
-      <AddTaskForm />
-      <TaskList />
-    </>
+    <div style={{ padding: "20px" }}>
+
+      <nav style={{ marginBottom: "20px", display: "flex", gap: "10px" }}>
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            padding: "6px 12px",
+            borderRadius: "6px",
+            textDecoration: "none",
+            backgroundColor: isActive ? "#7955cd" : "#eee",
+            color: isActive ? "#fff" : "#000",
+          })}
+        >
+          Список задач
+        </NavLink>
+        <NavLink
+          to="/stats"
+          style={({ isActive }) => ({
+            padding: "6px 12px",
+            borderRadius: "6px",
+            textDecoration: "none",
+            backgroundColor: isActive ? "#7955cd" : "#eee",
+            color: isActive ? "#fff" : "#000",
+          })}
+        >
+          Статистика
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddTaskForm />
+              <TaskList />
+            </>
+          }
+        />
+
+        <Route path="/stats" element={<TaskStats />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
   )
 }
 
